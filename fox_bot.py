4db4eafe4fee018405c0b59a21d6c887fc5e9000
kawanaga555 <kawanaga.neko@gmail.com>
2020-07-22 21:08:25 +0800

    Create fox_bot.py

---
+++ fox_bot.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import random
+import json
+import os
+
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+bot = commands.Bot(command_prefix='狐狸')
+
+@bot.event
+async def on_ready():
+    print(">>bot is online<<")
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f'Loaded {extension} down!')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f'unLoaded {extension} down!')
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f'reLoaded {extension} down!')
+
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
+
+if __name__ == "__main__":
+    bot.run(jdata['TOKEN'])
